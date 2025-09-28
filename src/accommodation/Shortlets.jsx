663b53f991de295a1f7d68adba2fb0b4fcae0f83
Azeezfasasi <fas.azeez@gmail.com>
2025-09-28 06:34:30 +0100

    fixed short let hero

--- src/accommodation/Shortlets.jsx
+++ src/accommodation/Shortlets.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import FooterSection from '../assets/components/home-components/FooterSection'
-import ShortLetHero from '../assets/components/category-components/accommodation-component/ShoRtLetHero'
 import ShortLetList from '../assets/components/category-components/accommodation-component/ShortLetList'
+import ShortLetHero from '../assets/components/category-components/accommodation-component/ShortLetHero'
+
 
 export default function ShortLets() {
   return (
