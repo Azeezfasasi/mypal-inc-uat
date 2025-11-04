3a5c82916c402a60abbd8bf0f4a90e1ba6ec50a7
Azeezfasasi <fas.azeez@gmail.com>
2025-11-04 20:54:03 +0100

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { HelmetProvider } from "react-helmet";
 
 const queryClient = new QueryClient();
 
@@ -12,9 +11,9 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <HelmetProvider>
+        {/* <HelmetProvider> */}
           <App />
-        </HelmetProvider>
+        {/* </HelmetProvider> */}
       </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
